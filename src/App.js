cdf676c6945c8ac86504bd4111a5263837337b35
mohonmia <mohonmia97@gmail.com>
2020-03-11 15:48:16 +0600

    react core cocepts

--- src/App.js
+++ src/App.js
@@ -3,24 +3,52 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let fname = "Mohon";
+  let lname = "Mia";
+  let testStylesheet = {
+    background:'red',
+    padding: "10px",
+    borderRadius: '5px'
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <SmilarStaticData></SmilarStaticData>
+      <SmilarStaticData></SmilarStaticData>
+
+      <SimilarPaternDynami name="Mohon Mia" desg="web developer"></SimilarPaternDynami>
+      <SimilarPaternDynami name="Nazrul Islam" desg="Froentend developer"></SimilarPaternDynami>
       </header>
     </div>
   );
 }
 
+function SmilarStaticData(){
+  const PersonStylesheet = {
+    backgroundColor: 'red',
+    color: '#fff',
+    padding: '5px',
+    marginTop:'5px'
+  }
+  return(
+  <div style={PersonStylesheet}>
+    <h1 style={{margin:'0',padding:'0'}}>Component Pattern</h1>
+    <h2 style={{margin:'0',padding:'0'}}>Another Component Pattern</h2>
+  </div>);
+}
+
+function SimilarPaternDynami(props){
+  let dyanmicStylesheel = {
+    backgroundColor: 'yellow',
+    color: 'green',
+    padding: '5px',
+    marginTop: '5px'new-cars | condition, used-cars | condition,
+  }
+  return(
+    <div>
+      <h1 style={dyanmicStylesheel}>Name:{props.name}</h1>
+      <h4 style={dyanmicStylesheel}>Designation:{props.desg}</h4>
+    </div>
+  );
+}
 export default App;
